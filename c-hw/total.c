76778c570c695b57ee74ce8c07635bda312eab5f
Beth <iyara@materiamagica.com>
2015-02-28 01:59:29 -0600

    revised for ex6-3 to handle negative numbers as input

--- c-hw/total.c
+++ c-hw/total.c
@@ -1,14 +1,16 @@
-/* example 6-2, pg 90 */
+/* example 6-3, pg 90, "totalb.c" */
 /* enter a series of numbers, keeping a running total, end and print total when zero is entered */
 
 #include <stdio.h>
 char line[100];     /* line of data for input */
 int total;          /* running total */
 int item;           /* next item to add */
+int minus_items;    /* number of negative items */
 
 int main()
 {
     total = 0;
+    minus_items = 0;
     while (1)  {
         printf("Enter # to add \n");
         printf("  or 0 to stop:");
@@ -19,9 +21,15 @@ int main()
         if (item == 0)
             break;
 
+        if (item < 0) {
+            ++minus_items;
+            continue;
+        }
+
         total += item;
         printf("Total: %d\n", total);
     }
     printf("Final total %d\n", total);
+    printf("with %d negative items omitted\n", minus_items);
     return (0);
 }
